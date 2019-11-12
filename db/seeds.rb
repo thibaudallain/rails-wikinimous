5a5c88bb62c5ef4578de8a6bd3465cf4901971f4
Thibaud Allain <thibaudallainta@gmail.com>
2019-11-12 20:16:20 +0100

    wiki done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  new_article = Article.new(title: Faker::ProgrammingLanguage.name, content: Faker::Hipster.sentence)
+  new_article.save
+end
